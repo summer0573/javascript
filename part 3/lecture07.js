ec9151459f368e64f11d925bfee0f1daa1a12621
summer0573 <83990983+summer0573@users.noreply.github.com>
2022-02-23 15:55:28 +0900

    Add files via upload

--- part 3/lecture07.js
+++ part 3/lecture07.js
@@ -1,7 +1,7 @@
-var empty_obj = {};
+var empty_obj = {}; //객체 생성(값 없음)
 undefined
 typeof(empty_obj);
-'object'
+'object' //단순 자료형보다 더 복잡한 자료를 표현할 때 사용
 var man = {name : "정현진", age : 18, height : 170};
 undefined
 man.name;

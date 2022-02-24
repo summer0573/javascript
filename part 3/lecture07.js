dfd162f3154a479fedc2151c2571852d2d0ef0e3
summer0573 <83990983+summer0573@users.noreply.github.com>
2022-02-24 15:57:44 +0900

    Add files via upload

--- part 3/lecture07.js
+++ part 3/lecture07.js
@@ -1,14 +1,15 @@
-var empty_obj = {}; //객체 생성(값 없음)
-undefined
-typeof(empty_obj);
-'object' //단순 자료형보다 더 복잡한 자료를 표현할 때 사용
-var man = {name : "정현진", age : 18, height : 170};
-undefined
-man.name;
-'정현진'
-man["name"];
-'정현진'
-man.name="배서연";
-'배서연'
-man["age"]=3;
-3
+//개발자 도구
+// var empty_obj = {}; //객체 생성(값 없음)
+// undefined
+// typeof(empty_obj);
+// 'object' //단순 자료형보다 더 복잡한 자료를 표현할 때 사용
+// var man = {name : "정현진", age : 18, height : 170};
+// undefined
+// man.name;
+// '정현진'
+// man["name"];
+// '정현진'
+// man.name="배서연";
+// '배서연'
+// man["age"]=3;
+// 3
--- part 3/lecture08.js
+++ part 3/lecture08.js
@@ -1,24 +1,25 @@
-var undefined_var; //변수나 속성이 정의되지 않은 경우를 표현
+//개발자 도구
+// var undefined_var; //변수나 속성이 정의되지 않은 경우를 표현
 
-var obj={
-    x:1, y:2
-};
+// var obj={
+//     x:1, y:2
+// };
 
-undefined_var;
-undefined
-typeof(CSSUnitValue_var);
-'undefined'
-obj.x
-1
-obj.y
-2
-obj.z
-undefined
-typeof(obj.z)
-'undefined'
-var null_var; //의도적으로 비어있는 상태를 표현
-undefined
-null_var=null;
-null
-typeof(null_var)
-'object'
+// undefined_var;
+// undefined
+// typeof(CSSUnitValue_var);
+// 'undefined'
+// obj.x
+// 1
+// obj.y
+// 2
+// obj.z
+// undefined
+// typeof(obj.z)
+// 'undefined'
+// var null_var; //의도적으로 비어있는 상태를 표현
+// undefined
+// null_var=null;
+// null
+// typeof(null_var)
+// 'object'

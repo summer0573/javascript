30d010838953769aa67f8774e2da7de8b34a4ca1
summer0573 <83990983+summer0573@users.noreply.github.com>
2022-02-27 15:30:29 +0900

    Add files via upload

---
+++ part 8/example2.js
@@ -0,0 +1,5 @@
+if(false){
+    console.log("1");
+}else{ //위의 if문이 실행됨에 따라 실행되지 않거나 실행됨
+    console.log("2");
+}
---
+++ part 8/switch1.js
@@ -0,0 +1,27 @@
+console.log("MENU");
+console.log("1. 아이스 아메리카노");
+console.log("2. 카페라떼");
+console.log("3. 카푸치노");
+console.log("4. 아샷추");
+
+var choice = parseInt(prompt("메뉴를 선택해 주세요."));
+
+console.log(choice+"번 메뉴를 선택하셨습니다.")
+
+switch(choice){
+    case 1 :
+        console.log("아이스 아메리카노는 3500원 입니다.");
+        break; //switch문 빠져나오기
+    case 2 :
+        console.log("카페라떼는 4000원 입니다.");
+        break;
+    case 3 :
+        console.log("카푸치노는 4500원 입니다.");
+        break;
+    case 4 :
+        console.log("아샷추는 3500원 입니다.");
+        break;
+    defualt :
+        console.log("주문하신 메뉴는 없습니다.");
+        break;
+}

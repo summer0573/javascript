376b0b76b9a008f235a953999531e8bc03f7e0ed
summer0573 <83990983+summer0573@users.noreply.github.com>
2022-02-28 16:08:33 +0900

    Add files via upload

---
+++ part 9/for.js
@@ -0,0 +1,15 @@
+var array = [1,2,3,4,5,6,7,8,9,10];
+
+// var i=0;
+// while(i<array.length) {
+//     console.log(array[i]);
+//     i++;
+// }
+// console.log("while문 실행");
+
+for(var i=0; i<array.length; i++){
+    console.log(array[i]);
+}
+console.log("for문 실행");
+
+// continue = 반복실행 코드의 끝으로 이동한 다음, 바로 조건식 검사
